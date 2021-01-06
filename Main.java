8f373a9e834fd71640ba8236827a7aca837f757a
1nvald0ne <sterlingmccrevey11@protonmail.com>
2021-01-06 17:11:12 -0600

    added and created main java file

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+public static void main (String args[]){
+  //start creating code here
+  say("Authors: ");
+}
+private static void say (String s){
+  System.out.println(s);
+}
+}
