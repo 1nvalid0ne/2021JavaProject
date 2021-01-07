7ed3e821c118782886b93628c3c446ba919cac94
1nvald0ne <sterlingmccrevey11@protonmail.com>
2021-01-06 20:19:27 -0600

    generated legitimate java source file

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//establishing functions
+		Scanner input = new Scanner(System.in);
+		// general opening text
+		say("Authors: Sterling McCrevey & Mason Rhodes... \n"
+				+ "");
+
+	}
+private static void say (String s) {
+	System.out.println(s);
+}
+}
