c9b02cd0e36a222c01f51865b63f8c0b7285557a
1nvald0ne <sterlingmccrevey11@protonmail.com>
2021-01-06 19:21:25 -0600

    added brainstorming document and edited credentials of the java program

--- Main.java
+++ Main.java
@@ -1,8 +1,13 @@
+import java.util.*
+import java.io.*
 public class Main {
 
 public static void main (String args[]){
-  //start creating code here
-  say("Authors: ");
+  //defining scanners and other functions
+  Scanner input = new Scanner(System.in);
+  //general opening text
+  say("Authors: Sterling McCrevey & Mason Rhodes...\n "
+  + " ");
 }
 private static void say (String s){
   System.out.println(s);
